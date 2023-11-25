924190936e12710d7d372eeabc86b9916b907035
eleduard0 <arturo.13040570@gmail.com>
2023-11-24 23:46:13 -0700

    Modelo

---
+++ Modelos/Bicicletas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoV2
+{
+    public class Bicicletas
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Tamaño { get; set;}
+        public string Talla { get; set;}
+    }
+}
